8e1ef6ede0115c8cf09ca028db17fbd25eea7c25
kiran <kiran.jakka@acknotech.com>
2017-09-08 15:53:59 +0530

    for pipeline testing

--- src/main/java/com/websystique/springmvc/configuration/CORSFilter.java
+++ src/main/java/com/websystique/springmvc/configuration/CORSFilter.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 public class CORSFilter implements Filter {
 
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
-		System.out.println("Filtering on push into server...........................................................");
+		System.out.println("Filtering on push into server............................................................");
 		HttpServletResponse response = (HttpServletResponse) res;
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Credentials", "true");
